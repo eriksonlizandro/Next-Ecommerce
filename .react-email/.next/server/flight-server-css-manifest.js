daf263c455dcac7433fb9a2e43e59b1261045142
eriksongonzalez <egonzalez@wanderu.com>
2023-10-26 20:01:10 -0600

    email

---
+++ .react-email/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,24 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/app/preview/[slug]/page": [
+      "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/styles/globals.css",
+      "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ],
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/app/page": [
+      "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/styles/globals.css",
+      "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/app/layout.tsx": [
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/styles/globals.css"
+  ],
+  "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/app/preview/[slug]/page.tsx": [
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/styles/globals.css"
+  ],
+  "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/app/page.tsx": [
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "/Users/eriksonlizandro/Progra/practice/next-ecommerce/.react-email/src/styles/globals.css"
+  ]
+}
